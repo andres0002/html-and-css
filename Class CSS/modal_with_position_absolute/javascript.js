9082166c44d4302e214b4dcb52fdfc29bd67a7a9
William Andres Ramirez Jimenez <frivandres2011@hotmail.com>
2025-01-15 19:08:25 -0500

    WR: Update.

---
+++ Class CSS/modal_with_position_absolute/javascript.js
@@ -0,0 +1,6 @@
+// Cierra el modal desde el btn (x).
+document.querySelector(".btn--close-modal").addEventListener("click", () => { document.querySelector(".modal").style.display = "none" })
+// Cierra el modal desde el btn (Aceptar).
+document.querySelector(".btn--acept").addEventListener("click", () => { document.querySelector(".modal").style.display = "none" })
+// Cierra el modal desde el btn (Cancelar).
+document.querySelector(".btn--cancel").addEventListener("click", () => { document.querySelector(".modal").style.display = "none" })
